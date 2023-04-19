060797389b3a995c418a676f8b4f47a562e66e3f
TheNathanSpace <nathan@thenathan.space>
2023-04-19 12:09:50 -0600

    Finished table work I think

--- 6-map_fips_codes.sql
+++ 6-map_fips_codes.sql
@@ -10,12 +10,25 @@ CREATE TABLE fips_map (
 
 -- Insert joined rows from the other two FIPS tables
 INSERT INTO fips_map
-SELECT city_fips.state_abb, city_fips.city, county_fips, city_fips.city_fips
+SELECT DISTINCT city_fips.state_abb, city_fips.city, county_fips, city_fips.city_fips
 FROM city_fips JOIN county_city_fips ON city_state_name LIKE  '%' || city || '%' || ', ' || state_abb;
 
 DROP TABLE city_fips;
 DROP TABLE county_city_fips;
 
+-- Remove duplicate entries (one city can have multiple counties, but this doesn't help us) 
+DELETE FROM fips_map
+WHERE (state_abb, city, county_fips, city_fips) NOT IN (
+        SELECT
+                MAX(state_abb),
+                MAX(city),
+                MAX(county_fips),
+                MAX(city_fips)
+        FROM fips_map
+        GROUP BY city_fips
+);
+
+-- Add primary key to fips_map
 ALTER TABLE fips_map
-ADD CONSTRAINT fips_map_pkey 
-PRIMARY KEY (state_abb, city, county_fips, city_fips);
+ADD CONSTRAINT fips_map_pkey
+PRIMARY KEY (city_fips);
--- 7-add_foreign_keys.sql
+++ 7-add_foreign_keys.sql
@@ -1,9 +1,11 @@
--- Add necessary foreign keys
+-- Remove unusable entries in obesity_data_cleaned (where there's no associated income_data point)
+DELETE FROM obesity_data_cleaned
+WHERE city_fips NOT IN (
+	SELECT city_fips
+	FROM fips_map
+);
+
+-- Add city_fips foreign key between obesity_data_cleaned and fips_maps 
 ALTER TABLE obesity_data_cleaned
 ADD CONSTRAINT city_fips_fkey
 FOREIGN KEY (city_fips) REFERENCES fips_map(city_fips);
-
-ALTER TABLE income_data
-ADD CONSTRAINT county_fips_fkey
-FOREIGN KEY (county_fips) REFERENCES fips_map(county_fips);
-
